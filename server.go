f643a6ba81f3da14de238e5f960d2ce5ea2a4339
Atsushi Hara <toohsk@gmail.com>
2018-03-23 11:28:08 +0900

    Add routing examples.

--- server.go
+++ server.go
@@ -11,5 +11,25 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	// You can route with REST METHODS, like e.GET or e.POST, e.PUT, e.DELETE.
+	// For example, we route getUser method with GET method.
+	e.GET("/users/:id", getUser)
+
+	// If you need query parameters in url, example is in show function.
+	e.GET("/show", show)
+
 	e.Logger.Fatal(e.Start(":1323"))
+}
+
+func getUser(c echo.Context) error {
+	// User ID from path `users/:id`
+	id := c.Param("id")
+	return c.String(http.StatusOK, id)
+}
+
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
