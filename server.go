40b30d8f0f5a2ca9ea5a9369e68b6a7bbb4d3740
Atsushi Hara <toohsk@gmail.com>
2018-03-26 11:21:56 +0900

    Add post example.

--- server.go
+++ server.go
@@ -18,6 +18,9 @@ func main() {
 	// If you need query parameters in url, example is in show function.
 	e.GET("/show", show)
 
+	// If you are posting values from form, example is in save function.
+	e.POST("/save", save)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
@@ -32,4 +35,11 @@ func show(c echo.Context) error {
 	team := c.QueryParam("team")
 	member := c.QueryParam("member")
 	return c.String(http.StatusOK, "team:"+team+", member:"+member)
+}
+
+func save(c echo.Context) error {
+	// Get name and email from form value
+	name := c.FormValue("name")
+	email := c.FormValue("email")
+	return c.String(http.StatusOK, "name:"+name+", email:"+email)
 }
